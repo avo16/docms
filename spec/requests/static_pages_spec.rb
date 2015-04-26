cba1ca59a875c69a2acc4e13b54b2ee523107e71
Avram Oana <avram.oana16@gmail.com>
2015-04-26 20:57:39 +0300

    Added links and some CSS

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -1,10 +1,47 @@
-require 'rails_helper'
+require 'spec_helper'
 
-RSpec.describe "StaticPages", type: :request do
-  describe "GET /static_pages" do
-    it "works! (now write some real specs)" do
-      get static_pages_home_path
-      expect(response).to have_http_status(200)
-    end
-  end
+describe "Static pages"
+
+	describe "Home page" do 
+
+		it "should have the h1 'Document Management System'" do
+			visit 'static_pages/home'
+			page.should have_selector('h1', :text => 'Document Management System')
+		
+		end
+
+		it "should have the title 'Docms | Home'" do
+			visit 'static_pages/home'
+			page.should have_selector('title', :text => "Docms | Home")
+		end
+	end
+
+
+	describe "Help page" do 
+
+		it "should have the h1 'Help'" do
+			visit 'static_pages/help'
+			page.should have_selector('h1', :text => 'Help')
+		
+		end
+
+		it "should have the title 'Docms | Help'" do
+			visit 'static_pages/help'
+			page.should have_selector('title', :text => "Docms | Help")
+		end
+	end
+
+
+	describe "About page" do 
+
+		it "should have the h1 'About us'" do
+			visit 'static_pages/about'
+			page.should have_selector('h1', :text => 'About us')
+		
+		end
+
+		it "should have the title 'Docms | About Us'" do
+			visit 'static_pages/about'
+			page.should have_selector('title', :text => "Docms | About Us")
+		end
 end
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -20,6 +20,14 @@ RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
   # assertion/expectation library such as wrong or the stdlib/minitest
   # assertions if you prefer.
+
+  #enable should
+  config.expect_with :rspec do |c|
+    c.syntax = :should
+  end
+
+
+
   config.expect_with :rspec do |expectations|
     # This option will default to `true` in RSpec 4. It makes the `description`
     # and `failure_message` of custom matchers include text for helper methods
@@ -84,4 +92,28 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+end
+
+
+# This file is copied to spec/ when you run 'rails generate rspec:install'
+ENV["RAILS_ENV"] ||= 'test'
+require File.expand_path("../../config/environment", __FILE__)
+require 'rspec/rails'
+
+
+# Requires supporting ruby files with custom matchers and macros, etc,
+# in spec/support/ and its subdirectories.
+Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
+
+RSpec.configure do |config|
+  # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
+  # config.fixture_path = "#{::Rails.root}/spec/fixtures"
+  config.use_transactional_fixtures = true
+  config.infer_base_class_for_anonymous_controllers = false
+  config.order = "random"
+  # Include path helpers
+  config.include Rails.application.routes.url_helpers
+
+  config.include Capybara::DSL
+
 end
